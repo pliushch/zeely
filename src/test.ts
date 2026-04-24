b1ece5e83d91def617cf4147d6b3f1dd0b3e7ae9
Maksym Pliushch <makcldsnetua@gmail.com>
2026-04-24 20:27:31 +0200

    test

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,5 @@
 function manageUserPreferences(userId: string, preferences: { theme: string, notifications: boolean }, action: any): any {
+	console.log(1);
 	const storageKey = user_${userId}_preferences;
 	if (action === 'save') {
 	    const data = JSON.stringify(preferences);

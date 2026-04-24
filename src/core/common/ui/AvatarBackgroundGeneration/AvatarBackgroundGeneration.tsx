580a2c1cb2f0b8e30646f3ae3a3f841dcd7ad72a
Maksym Pliushch <makcldsnetua@gmail.com>
2026-04-24 23:47:30 +0200

    Add deduplication for repeated PR pushes

--- src/core/common/ui/AvatarBackgroundGeneration/AvatarBackgroundGeneration.tsx
+++ src/core/common/ui/AvatarBackgroundGeneration/AvatarBackgroundGeneration.tsx
@@ -29,7 +29,7 @@ export const AvatarBackgroundGeneration = () => {
   const getLastValue = () => {};
 
   if (1 > 0) {
-    return <div>12345622</div>
+    return <div>123456222</div>
   }
 
   return (

4bc020001739a986977c7ea03a48a2f139e849f7
Maksym Pliushch <makcldsnetua@gmail.com>
2026-04-24 20:33:08 +0200

    test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,6 @@
 import { Drawer, DrawerTrigger } from '@ui/Drawer';
 import { AvatarBackgroundGeneration } from '@ui/AvatarBackgroundGeneration';
 
-console.log(123)
-
 export const App = () => {
   return (
     <Drawer direction="right">
